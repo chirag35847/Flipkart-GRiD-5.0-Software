b39a13c0759741fed4eff99b61371258fc30ea40
Chirag Mittal <78473136+chirag35847@users.noreply.github.com>
2023-08-19 12:27:19 +0530

    changed icons on user portfolio screen

--- client/src/components/UserDash/Information.jsx
+++ client/src/components/UserDash/Information.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useUserDataContext } from "../../contexts/UserContextProvider";
 import InformationCard from "./InformationCard";
 import numeral from "numeral";
+import {IconBuildingBank, IconCoins,IconCurrencyEthereum,IconCurrencyBitcoin,IconShare3 } from '@tabler/icons-react'
+
 const Information = () => {
   const { user } = useUserDataContext();
   console.log("user", user);
@@ -19,26 +21,31 @@ const Information = () => {
         title={"Total Ether Spent"}
         value={numeral(user?.totalEtherSpent).format("0.0a")}
         displayButton={false}
+        icon={<IconCurrencyEthereum className="w-10 h-10"/>}
       />
       <InformationCard
         title={"Total Token Rewards"}
         value={numeral(user?.totalTokenRewards).format("0.0a")}
         displayButton={false}
+        icon={<IconCurrencyBitcoin className="w-10 h-10"/>}
       />
       <InformationCard
         title={"Number of Refferals"}
         value={numeral(user?.numberOfRefferrels).format("0.0a")}
         displayButton={false}
+        icon={<IconShare3 className="w-10 h-10"/>}
       />
       <InformationCard
         title={"Total Loyality Tokens"}
         value={numeral(user?.totalLoyalityTokenBalance).format("0.0a")}
         displayButton={true}
+        icon={<IconCoins className="w-10 h-10"/>}
       />
        <InformationCard
         title={"Total Balance"}
         value={numeral(user?.totalBalance).format("0.0a")}
         displayButton={false}
+        icon={<IconBuildingBank className="w-10 h-10"/>}
       />
     </SimpleGrid>
   );
